9f492bf1980ddec7b3d9812828bff13cae1b6a7f
Andrei Timotin <timotin.andrei@justconsult.md>
2024-07-29 14:37:13 +0300

    feat: Update deploy.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-    app: {
-        baseURL: '/change-md/'
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    $production: {
+        app: {
+            baseURL: '/change-md/'
+        },
     },
-  modules: ["@nuxtjs/tailwindcss"]
+    $development: {
+        //
+    },
+    modules: ["@nuxtjs/tailwindcss"]
 })
